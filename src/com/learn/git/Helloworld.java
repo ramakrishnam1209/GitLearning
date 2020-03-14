70f6c94bea45d5d6c4b4e0f61e8d591f6fd9918c
basava <basavapunnaiah81@gmail.com>
2020-03-14 23:54:00 +0530

    age check code added in HelloWorld file

--- src/com/learn/git/Helloworld.java
+++ src/com/learn/git/Helloworld.java
@@ -1,5 +1,7 @@
 package com.learn.git;
 
+import java.util.Scanner;
+
 public class Helloworld {
 
 	public static void main(String[] args) {
@@ -7,8 +9,16 @@ public class Helloworld {
 
 		System.out.println("welcome to git");
 		
-		System.out.println("HELLO GOOD MORNING!");
-		System.out.println("srinu srinu");
-	}
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter your age");
+		
+		int age=sc.nextInt();
+		if(age>=18) {
+			System.out.println("your eligible to vote");
+		}else {
+			System.err.println("your not eligible to vote because your age is less than 18 years");
+		}
+		
+		}
 
 }
