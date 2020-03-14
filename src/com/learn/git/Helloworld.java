6c21e10331f11f66501aeb5be3b79747ee9b0bf7
Basava <basava11140@gmail.com>
2020-03-15 00:09:49 +0530

    chetlu message added

--- src/com/learn/git/Helloworld.java
+++ src/com/learn/git/Helloworld.java
@@ -7,6 +7,7 @@ public class Helloworld {
 
 		System.out.println("welcome to git");
 		
+		System.out.println("chetlanu natandi galini varshanni pondandi");
 		System.out.println("HELLO GOOD MORNING!");
 		System.out.println("srinu srinu");
 	}
