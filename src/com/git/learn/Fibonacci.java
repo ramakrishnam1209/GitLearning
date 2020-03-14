f7092155675f9b8549aa49bb013a718ef28bc109
Basava <basava11140@gmail.com>
2020-03-14 23:16:11 +0530

    fibonacci file created

---
+++ src/com/git/learn/Fibonacci.java
@@ -0,0 +1,20 @@
+package com.git.learn;
+
+import java.util.Arrays;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+
+		int[] series=new int[20];
+		series[0]=0;
+		series[1]=1;
+		
+		for(int i=2;i<series.length;i++){
+			series[i]=series[i-2]+series[i-1];
+		}
+		
+		System.out.println(Arrays.toString(series));
+	}
+
+}
